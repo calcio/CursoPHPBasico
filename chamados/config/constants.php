a8d554bdc51c494636328b254247bab078a40320
Cálcio <calcionit@gmail.com>
2015-09-12 17:51:36 -0300

    movendo chamados

--- chamados/config/constants.php
+++
@@ -1,7 +0,0 @@
-<?php
-define('SITE_TITLE', 'Sistema de gerenciador de atividades');
-define('SUBTITLE', 'Gerenciador de atividades');
-define('VERSION', 'v.: 1.0');
-define('DS', DIRECTORY_SEPARATOR);
-$dir = explode('/config', dirname(__FILE__));
-define('BASE_PATH', $dir[0] . DS);
